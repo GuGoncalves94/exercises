2afc273fa4ea04478d5f25f4d4ca49e8c5de2674
Rodrigo <165202041+Digao-paiva@users.noreply.github.com>
2024-06-10 18:05:57 -0300

    Update 128.c

--- 128.c
+++ 128.c
@@ -8,3 +8,28 @@ para mais 10 posições.
 • Repita o procedimento de expandir dinamicamente com mais 10 valores sempre que o vetor estiver
 cheio. */
 
+//inicio do exercicio 128
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+	int i, **p, lin, col;
+	
+	scanf("%d %d", &lin, &col);
+	
+	
+	p = (int **)malloc(col * sizeof(int * ));
+	p[0] = (int *)malloc (lin * col * sizeof(int));
+	
+	printf("antes");
+	for(i = 1; i < col; i++) {
+		p[i] = p[i - 1] + col;
+		printf("dentro");
+	}
+		
+	
+	
+		
+	return 0;
+}
