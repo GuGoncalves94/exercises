dd8c612e4c2806104effc1afa55ab42c0b46b65b
Rodrigo <165202041+Digao-paiva@users.noreply.github.com>
2024-06-12 18:33:31 -0300

    Update 137.c

--- 137.c
+++ 137.c
@@ -1,3 +1,21 @@
 /* Faça uma função que receba um número N e retorne a soma dos algarismos do fatorial de N. No Moodle,
 insira apenas a sua função, que deve ter a seguinte assinatura:
 int somafat(int n) */
+
+#include<stdio.h>
+
+int somafat(int n);
+
+int main() {
+  int n;
+
+  scanf("%d", &n);
+
+  return 0;
+}
+
+int somafat(int n) {
+
+
+  return soma;
+}
